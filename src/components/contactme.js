fe03ee7395deeb9cb802c0f0fdc868a2f227e7e6
miguelmagsaysay <miggymagsy@gmail.com>
2023-09-02 00:30:50 +0800

    Applied responsive styles to Contact component

--- src/components/contactme.js
+++ src/components/contactme.js
@@ -3,7 +3,7 @@ import "./ContactStyles.css";
 
 function Contact() {
   const linkedin = "Miguel Jose Magsaysay";
-  const github = "Miguel Magsaysay";
+  const github = "@miguelmagsaysay";
   const contactNumber = "+63 916 306 0307";
 
   return (
@@ -11,13 +11,15 @@ function Contact() {
       <h1 className="contact-heading">Contact Information</h1>
       <ul className="contact-list">
         <li className="contact-item">
+          <i className="material-icons material-icon">account_circle</i>
           <strong>LinkedIn:</strong> {linkedin}
         </li>
         <li className="contact-item">
+          <i className="material-icons material-icon">code</i>
           <strong>GitHub:</strong> {github}
         </li>
         <li className="contact-item">
-          <span class="material-symbols-outlined"></span>
+          <i className="material-icons material-icon">phone</i>
           <strong>Contact Number:</strong> {contactNumber}
         </li>
       </ul>
@@ -26,4 +28,3 @@ function Contact() {
 }
 
 export default Contact;
-
